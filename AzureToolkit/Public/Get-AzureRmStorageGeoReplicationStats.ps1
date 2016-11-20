9965e22be66b5c09ace5819122bf1d976a6fb36d
Rupert Benbrook <rupertb@microsoft.com>
2016-11-20 14:29:39 +0000

    Added verbose logging

--- AzureToolkit/Public/Get-AzureRmStorageGeoReplicationStats.ps1
+++ AzureToolkit/Public/Get-AzureRmStorageGeoReplicationStats.ps1
@@ -9,16 +9,22 @@ function Get-AzureRmStorageGeoReplicationStats {
     )
 
     process {
+        Write-Verbose "Getting seconary endpoint context for storage account '$StorageAccountName'"
         $secondary = Get-AzureRmStorageContextFromAccountName -StorageAccountName $StorageAccountName -SecondaryEndpoints
 
+        Write-Verbose "Getting blob geo-replication stats for storage account '$StorageAccountName'"
         $client = $secondary.StorageAccount.CreateCloudBlobClient()
         $blobStats = ($client.GetServiceStats()).GeoReplication
+
+        Write-Verbose "Getting table geo-replication stats for storage account '$StorageAccountName'"
         $client = $secondary.StorageAccount.CreateCloudTableClient()
         $tableStats = ($client.GetServiceStats()).GeoReplication
+
+        Write-Verbose "Getting queue geo-replication stats for storage account '$StorageAccountName'"
         $client = $secondary.StorageAccount.CreateCloudQueueClient()
         $queueStats = ($client.GetServiceStats()).GeoReplication
 
-        return = (New-Object -TypeName PSObject -Property @{
+        return (New-Object -TypeName PSObject -Property @{
             Endpoint = "Blob"; Status = $blobStats.Status; LastSyncTime = $blobStats.LastSyncTime
         }), (New-Object -TypeName PSObject -Property @{
             Endpoint = "Table"; Status = $tableStats.Status; LastSyncTime = $tableStats.LastSyncTime
