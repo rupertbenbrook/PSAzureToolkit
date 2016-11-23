cb66d3c6b811026e5b57fb912aeb44f97a58ce5e
Rupert Benbrook <rupertb@microsoft.com>
2016-11-23 12:19:27 +0000

    Added a cache

--- AzureToolkit/Public/Get-AzureRegionPublicIpSubnets.ps1
+++ AzureToolkit/Public/Get-AzureRegionPublicIpSubnets.ps1
@@ -2,7 +2,21 @@ function Get-AzureRegionPublicIpSubnets {
 
     [CmdletBinding()]
     param (
+        [switch]$RefreshCache = $false
     )
+
+    $now = Get-Date
+    $regionSubnetsCache = $PSCmdlet.SessionState.PSVariable.Get("regionSubnetsCache").Value
+    $regionSubnetsCacheTime = $PSCmdlet.SessionState.PSVariable.Get("regionSubnetsCacheTime").Value
+    Write-Verbose "Last cached result was cached at $regionSubnetsCacheTime"
+    if (($RefreshCache -eq $false) -and
+        ($regionSubnetsCacheTime -ne $null) -and
+        ($regionSubnetsCache -ne $null) -and
+        ($now.Subtract($regionSubnetsCacheTime).TotalMinutes -lt 60)) {
+        
+        Write-Verbose "Returning result from cache that was cached at $regionSubnetsCacheTime"
+        return $regionSubnetsCache
+    }
 
     Write-Verbose "Downloading the Azure Datacenter IP ranges download confirmation page to find the latest XML file download"
     $downloadPage = Invoke-WebRequest -Uri "https://www.microsoft.com/download/confirmation.aspx?id=41653" -UseBasicParsing
@@ -15,7 +29,11 @@ function Get-AzureRegionPublicIpSubnets {
     $xmlFileUri = [string]$matches.Values[0]
     $response = Invoke-WebRequest -Uri $xmlFileUri -UseBasicParsing
     [xml]$xmlResponse = [System.Text.Encoding]::UTF8.GetString($response.Content)
-    $xmlResponse.AzurePublicIpAddresses.Region | %{
+    $regionSubnets = $xmlResponse.AzurePublicIpAddresses.Region | %{
         New-Object -TypeName PSObject -Property @{Region=$_.Name; Subnets=$_.IpRange.Subnet }
     }
+
+    $PSCmdlet.SessionState.PSVariable.Set("regionSubnetsCache", $regionSubnets)
+    $PSCmdlet.SessionState.PSVariable.Set("regionSubnetsCacheTime", $now)
+    return $regionSubnets
 }
