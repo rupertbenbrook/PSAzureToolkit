ed7dcf9cc739138314b0e82ca0fc1a4b09233e70
Rupert Benbrook <rupertb@microsoft.com>
2016-11-24 20:40:39 +0000

    Fixed param validation and pipeline, plus verbose log

--- AzureToolkit/Public/Add-AzureRmNetworkSecurityRuleConfigForRegionPublicIpSubnets.ps1
+++ AzureToolkit/Public/Add-AzureRmNetworkSecurityRuleConfigForRegionPublicIpSubnets.ps1
@@ -4,7 +4,7 @@ function Add-AzureRmNetworkSecurityRuleConfigForRegionPublicIpSubnets {
     param (
         [Parameter(Mandatory, ValueFromPipeline)]
         [ValidateNotNull()]
-        [Microsoft.Azure.Commands.Network.Models.PSEffectiveNetworkSecurityGroup]$NetworkSecurityGroup,
+        [Microsoft.Azure.Commands.Network.Models.PSNetworkSecurityGroup]$NetworkSecurityGroup,
 
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
@@ -17,15 +17,19 @@ function Add-AzureRmNetworkSecurityRuleConfigForRegionPublicIpSubnets {
         [string]$RuleNamePrefix,
 
         [Parameter(Mandatory)]
+        [ValidateRange(100, 4096)]
         [int]$StartingPriority,
 
         [Parameter(Mandatory)]
+        [ValidateSet("Allow", "Deny")]
         [string]$Access,
 
         [Parameter(Mandatory)]
+        [ValidateSet("Inbound", "Outbound")]
         [string]$Direction,
 
         [Parameter(Mandatory)]
+        [ValidateSet("Tcp", "Udp", "*")]
         [string]$Protocol,
 
         [Parameter(Mandatory)]
@@ -35,28 +39,39 @@ function Add-AzureRmNetworkSecurityRuleConfigForRegionPublicIpSubnets {
         [string]$Subnet
     )
 
-    $ranges = Get-AzureRegionPublicIpSubnets -RefreshCache $RefreshCache
+    begin {
+        Write-Verbose "Getting the list of Azure public IP subnets per region"
+        $ranges = Get-AzureRegionPublicIpSubnets -RefreshCache:$RefreshCache.IsPresent
 
-    [Microsoft.Azure.Commands.Network.Models.PSSecurityRule[]]$newRules = @()
-    $ranges | ? Region -eq $Region | Select-Object -ExpandProperty Subnets | %{ $index = $StartingPriority } {
-        $rule = New-Object -TypeName Microsoft.Azure.Commands.Network.Models.PSSecurityRule
-        $rule.Name = "$RuleNamePrefix$Region-$index";
-        $rule.Priority = "$index";
-        $rule.Access = $Access;
-        $rule.Direction = $Direction;
-        $rule.Protocol = $Protocol;
-        $rule.SourcePortRange = "*";
-        $rule.DestinationPortRange = $Port;
-        if ($Direction -eq "Inbound") {
-            $rule.SourceAddressPrefix = $_;
-            $rule.DestinationAddressPrefix = $Subnet;
+        if (-not ($Region -in ($ranges | Select-Object -ExpandProperty Region))) {
+            throw "The region $Region was not found in the Azure region public IP subnets"
         }
-        if ($Direction -eq "Outbound") {
-            $rule.SourceAddressPrefix = $Subnet;
-            $rule.DestinationAddressPrefix = $_;
-        }
-        $newRules += $rule
-        $index++
     }
-    $_.SecurityRules.AddRange($newRules)
+
+    process {
+        Write-Verbose "Adding rules for region $Region to network security group $($NetworkSecurityGroup.Id)"
+        [Microsoft.Azure.Commands.Network.Models.PSSecurityRule[]]$newRules = @()
+        $ranges | ? Region -eq $Region | Select-Object -ExpandProperty Subnets | %{ $index = $StartingPriority } {
+            $rule = New-Object -TypeName Microsoft.Azure.Commands.Network.Models.PSSecurityRule
+            $rule.Name = "$RuleNamePrefix$Region-$index";
+            $rule.Priority = "$index";
+            $rule.Access = $Access;
+            $rule.Direction = $Direction;
+            $rule.Protocol = $Protocol;
+            $rule.SourcePortRange = "*";
+            $rule.DestinationPortRange = $Port;
+            if ($Direction -eq "Inbound") {
+                $rule.SourceAddressPrefix = $_;
+                $rule.DestinationAddressPrefix = $Subnet;
+            }
+            if ($Direction -eq "Outbound") {
+                $rule.SourceAddressPrefix = $Subnet;
+                $rule.DestinationAddressPrefix = $_;
+            }
+            $newRules += $rule
+            $index++
+        }
+        $NetworkSecurityGroup.SecurityRules.AddRange($newRules)
+        return $NetworkSecurityGroup
+    }
 }
